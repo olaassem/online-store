81fbdda65e156da1f2243340697a8252f3491076
Ola Assem <ola.assem@viget.com>
2021-04-20 07:19:24 +0200

    initial cart model

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class CartRoute extends Route {
+  model() {
+    const items = [{ price: 10 }, { price: 110 }];
+    return items;
+  }
 }
