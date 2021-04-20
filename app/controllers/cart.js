ff4b4e31a9f85f21051ee6ec549e81d8696beacc
Ola Assem <ola.assem@viget.com>
2021-04-20 07:59:16 +0200

    cart controller get subtotal, tax, & subtotal

---
+++ app/controllers/cart.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+
+export default class CartController extends Controller {
+  get subtotal() {
+    return this.model.reduce((acc, item) => {
+      return acc + item.price;
+    }, 0);
+  }
+
+  get tax() {
+    return this.subtotal * 0.09;
+  }
+
+  get total() {
+    return this.subtotal + this.tax;
+  }
+}
