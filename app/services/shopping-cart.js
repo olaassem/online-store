98b714abb4d0b1b20a4d3029e82e03443c0f6220
Ola Assem <ola.assem@viget.com>
2021-04-22 19:36:08 +0200

    adds shoppingCart service, injects it, & renders itemList in cart template

---
+++ app/services/shopping-cart.js
@@ -0,0 +1,15 @@
+import Service from '@ember/service';
+
+export default class ShoppingCartService extends Service {
+  itemList = [
+    {
+      name: '1',
+    },
+    {
+      name: '2',
+    },
+    {
+      name: '3',
+    },
+  ];
+}
