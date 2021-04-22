dc3c53cd6686f71618abb99733ace24f480eaa84
Ola Assem <ola.assem@viget.com>
2021-04-22 04:02:53 +0200

    displays product images on index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import { products } from '../data';
+import { products } from '../data/products';
 
 export default class IndexRoute extends Route {
   model() {
