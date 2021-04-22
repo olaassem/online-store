0a2301fc1b2a58430cfcdb16c0d8ce97ce9474f8
Ola Assem <ola.assem@viget.com>
2021-04-22 04:44:59 +0200

    updates & passes currency helper to cart

--- app/helpers/currency.js
+++ app/helpers/currency.js
@@ -1,14 +1,16 @@
-import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
 
-export default helper(function currency(params, hash) {
-  const [number] = params;
-  const { sign } = hash;
-  const dollars = Math.floor(number);
-  let cents = Math.floor((number * 100) % 100);
+export default class currency extends Helper {
+  compute(params, hash = {}) {
+    const [number] = params;
+    const { sign = '$' } = hash;
+    const dollars = Math.floor(number);
+    let cents = Math.floor((number * 100) % 100);
 
-  if (cents.toString.length() === 1) {
-    cents = '0' + cents;
+    if (cents.toString().length === 1) {
+      cents = '0' + cents;
+    }
+
+    return `${sign}${dollars}.${cents}`;
   }
-
-  return `${sign}${dollars}.${cents}`;
-});
+}
