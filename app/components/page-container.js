f235ebe1381e5ded06f812763d92131c9e68a933
Ola Assem <ola.assem@viget.com>
2021-04-23 04:54:56 +0200

    updates item count method

--- app/components/page-container.js
+++ app/components/page-container.js
@@ -2,5 +2,11 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 
 export default class PageContainerComponent extends Component {
-  @service('shopping-cart') cart;
+  @service('shoppingCart') cart;
+
+  get itemCount() {
+    return this.cart.itemList.reduce((total, item) => {
+      return (total += item.count);
+    }, 0);
+  }
 }
